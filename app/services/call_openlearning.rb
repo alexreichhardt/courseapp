cff7260a90a5467fc669eb56b666fc2d4dffd85f
jakob wolff martinez <j.wolffmartinez@gmail.com>
2018-07-09 18:09:54 +0200

    added service class structure

---
+++ app/services/call_openlearning.rb
@@ -0,0 +1,8 @@
+class CallOpenlearning
+
+  def self.import
+    # This method should "translate" the corresponding platform API
+    # And create an instance of each course
+  end
+
+end
---
+++ app/services/call_udemy.rb
@@ -0,0 +1,8 @@
+class CallUdemy
+
+  def self.import
+    # This method should "translate" the corresponding platform API
+    # And create an instance of each course
+  end
+
+end
