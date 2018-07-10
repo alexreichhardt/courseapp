24b6f84ca30421825f5e97f2a27f2a35bce5a9a9
Bora Dirilgen <bora.c.dirilgen@gmail.com>
2018-07-10 11:17:15 +0200

    scraping the udacity api with httparty

--- app/services/udacity_importer.rb
+++ app/services/udacity_importer.rb
@@ -1,8 +1,15 @@
 class UdacityImporter
+  include HTTParty
 
   def self.call
     # This method should "translate" the corresponding platform API
     # And create an instance of each course
-  end
 
+    response = HTTParty.get('https://www.udacity.com/public-api/v0/courses')
+    response["courses"].each do |course|
+      puts course["title"]
+    end
+
+  end
 end
+
