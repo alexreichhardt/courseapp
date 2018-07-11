eeb4ab48d5ff0d387bf80f6b242e326a250b4b19
Bora Dirilgen <bora.c.dirilgen@gmail.com>
2018-07-11 17:58:25 +0200

    started search

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,7 +2,12 @@ class CoursesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :index]
 
   def index
-    @courses = Course.all
+
+    if params[:criteria][:search].nil? || params[:criteria][:search] == ""
+      @courses = Course.all
+    else
+      @courses = Course.search_global(params[:criteria][:search])
+    end
   end
 
   def show
--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,13 @@ class Course < ApplicationRecord
   enum knowledge_level: [:beginner, :intermediate, :advanced]
   has_many :bookmarks, dependent: :destroy
   has_many :course_reviews, dependent: :destroy
+  include PgSearch
+
+  pg_search_scope :search_global, against: [ :title, :description, :subtitle ],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
+
+  # pg_search_scope :search_by_title, against: :title
+  # pg_search_scope :search_by_description, against: :description
 end
