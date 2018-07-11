7770108d549e1d56388ecf37119d9a9cb1877332
jakob wolff martinez <j.wolffmartinez@gmail.com>
2018-07-11 18:19:53 +0200

    users can bookmark and debookmark courses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def html_button_to(html = nil, options = nil, html_options = nil)
+    button_to(options, html_options) do
+      html
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :courses, only: [ :index, :show ]
+  resources :courses, only: [ :index, :show ] do
+    resources :bookmarks, only: [ :new, :create, :destroy ]
+  end
 
 
   require "sidekiq/web"
