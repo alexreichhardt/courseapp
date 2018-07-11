a2d6965b5826eb3477fff16269d1fedd36432fa2
Robert Milekonian <bushbest@mail.ru>
2018-07-11 14:34:22 +0200

    seeds

--- app/services/call_openlearning.rb
+++ app/services/call_openlearning.rb
@@ -6,3 +6,5 @@ class CallOpenlearning
   end
 
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_09_153052) do
+ActiveRecord::Schema.define(version: 2018_07_10_130410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,8 +68,9 @@ ActiveRecord::Schema.define(version: 2018_07_09_153052) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "birthdate"
+    t.date "birth_date"
     t.string "avatar_image"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
