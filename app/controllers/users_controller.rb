2718481fdde56fa97262086d18fcfd16bd80e89d
Alexander Reichhardt <alexander.reichhardt@whu.edu>
2018-07-11 22:30:25 +0200

    navbar/ users(html/bookmarks)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,33 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    bookmark_ids = Bookmark.where(user_id: params[:id])
+    @courses = []
+    bookmark_ids.each do |id|
+      @courses << Course.where(id: id.course_id)
+    end
   end
+
+  def edit_avatar
+    @user = User.new
+  end
+
+  def new_avatar
+    @user = User.where(id: current_user.id)
+    @user.update(user_params)
+    redirect_to user_path(current_user.id)
+  end
+
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit( :avatar)
   end
 end
