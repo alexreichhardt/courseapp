8829abd935abb176a9b5e8e8678475b62cc8bc98
jakob wolff martinez <j.wolffmartinez@gmail.com>
2018-07-15 09:15:06 +0200

    db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,25 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :persist_last_visited_path
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def persist_last_visited_path
+    unless Rails.configuration.ignored_paths.include?(request.path) || request.xhr?
+     session[:last_visited_path] = request.path
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    if session[:last_visited_path].present?
+      session[:last_visited_path]
+    else
+      root_path
+    end
   end
 end
