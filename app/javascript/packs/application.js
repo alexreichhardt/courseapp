df3ec5f7c878a70667292257e042e7a52458c4e4
jakob wolff martinez <j.wolffmartinez@gmail.com>
2018-07-16 15:06:28 +0200

    implemented readmore jquery script

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,26 @@
+
+
 import "bootstrap";
 
-import { loadDynamicBannerText } from '../components/dynamictext';
-loadDynamicBannerText();
+import { readmore } from '../components/readmore';;
 
-import { pickTabs } from "../components/picktabs";
-pickTabs();
 
-import { categoryChoice } from "../components/categorychoice";
-categoryChoice();
 
-import { dataSlider } from '../components/dataslider';
-dataSlider();
+  import { loadDynamicBannerText } from '../components/dynamictext';
+  loadDynamicBannerText();
+
+  import { pickTabs } from "../components/picktabs";
+  pickTabs();
+
+  import { categoryChoice } from "../components/categorychoice";
+  categoryChoice();
+
+  // import { dataSlider } from '../components/dataslider';
+  // dataSlider();
+
+
+
+
+
 
 
