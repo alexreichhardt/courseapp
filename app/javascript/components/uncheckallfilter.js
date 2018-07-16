a277740a0d81151a8851b0ef29c1b1651add15e0
jakob wolff martinez <j.wolffmartinez@gmail.com>
2018-07-16 22:47:52 +0200

    fixed js bug

--- app/javascript/components/uncheckallfilter.js
+++ app/javascript/components/uncheckallfilter.js
@@ -1,6 +1,12 @@
-function uncheckAll(){
-   $('input[type="checkbox"]:checked').prop('checked',false);
-   $(this).removeClass("active");
-}
+// function uncheckAll(){
+//    $('input[type="checkbox"]:checked').prop('checked',false);
+//    $('.active').removeClass("active");
+// }
 
-$('.uncheck-all').on('click', uncheckAll())
+// $('.uncheck-all').on('click', uncheckAll())
+
+
+$(document).on('click', '.uncheck-all', function () {
+  $('input[type="checkbox"]:checked').prop('checked',false);
+  $('.active').removeClass("active");
+});
