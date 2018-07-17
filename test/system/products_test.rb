9877467765ac899116b30cf162a1d15d9901d69c
jakob wolff martinez <j.wolffmartinez@gmail.com>
2018-07-17 11:58:53 +0200

    fixed labeling issue in filter

---
+++ test/system/products_test.rb
@@ -0,0 +1,12 @@
+require "application_system_test_case"
+
+class ProductsTest < ApplicationSystemTestCase
+  test "visiting the index" do
+  #   visit products_url
+  #
+  check-links 'http://your-site.com'
+
+  #
+  #   assert_selector "h1", text: "Product"
+  end
+end
