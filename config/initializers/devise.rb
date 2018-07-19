8f73230db74ec1bcc77beda9e1f6890545f3243b
jakob wolff martinez <j.wolffmartinez@gmail.com>
2018-07-19 10:18:01 +0200

    added users and reviews to seedfile, fixed avatar link on show page

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'c80a55869474cfb20ca8c2659159277f1f008a0a0aade7af2b6aacc20b6cfb9499c1e4c541239eeaa175424dd82b8cabd6a82385a7063ffd23d419eb3c8248a1'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -163,7 +163,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 6..128
+  config.password_length = 3..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
